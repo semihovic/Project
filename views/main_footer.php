687695119bd5f5dd64cdebdbf047940bff477d61
semihovic <eren_doksanbir@live.be>
2015-12-11 17:03:36 +0100

    Create main_footer.php

---
+++ views/main_footer.php
@@ -0,0 +1,138 @@
+</div>
+<!-- ./page content holder -->
+</div>
+<!-- ./page content wrapper -->
+</div>
+<!-- ./page content -->
+
+<!-- page footer -->
+<footer>
+
+    <!-- page footer wrap -->
+    <div class="footer-wrap bg-dark-gray">
+        <!-- page footer holder -->
+        <div class="footer-holder footer-holder-main">
+
+            <div class="row">
+
+                <!-- about -->
+                <div class="col-md-3">
+                    <h3>About IC Clear</h3>
+                    <p>Clarity and IC Clear jointly hosted this year’s Clarity conference. It provided a unique opportunity for clear communication professionals and plain language experts from around the world to showcase their best teaching and learning practices. The focus was on the latest initiatives and research in clear communication in both the public and the private sector and emphasised legal language as a prime example.</p>
+                </div>
+                <!-- ./about -->
+
+                <!-- quick links -->
+                <div class="col-md-3">
+                    <h3>Partners</h3>
+
+                    <div class="list-links">
+                        <a href="http://www.thomasmore.be">Thomas More</a>
+                        <a href="http://www.sfu.ac.at/">Sigmund Freud Universität</a>
+                        <a href="http://portaal.eki.ee/">Eesti Keele Instituut</a>
+                        <a href="www.eadtu.nl">EADTU</a>
+                        <a href="www.isec.universitas.pt">Instituto Superior de Ciencias Educação</a>
+                        <a href="www.sfu.ca">Simon Fraser University</a>
+                    </div>                                
+                </div>
+                <!-- ./quick links -->
+
+                <!-- recent tweets -->
+                <div class="col-md-3">
+                    <h3>Recent Tweets</h3>
+
+                    <div class="list-with-icon small">
+                        <div class="item">
+                            <div class="icon">
+                                <span class="fa fa-twitter"></span>
+                            </div>
+                            <div class="text">
+                                <a href="#">@ICClear</a> Give us your vote. Conference @ICClear @ClarityInterna nominated as best...
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="icon">
+                                <span class="fa fa-twitter"></span>
+                            </div>
+                            <div class="text">
+                                <a href="#">@ICClear</a> The IC Clear Clarity2014 conference was nominated as...
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="icon">
+                                <span class="fa fa-twitter"></span>
+                            </div>
+                            <div class="text">
+                                <a href="#">@ICClear</a> Radio 1 Hautekiet lanceert campagne 'Heerlijk helder'...
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- ./recent tweets -->
+
+                <!-- contacts -->
+                <div class="col-md-3">
+                    <h3>Contacts</h3>
+
+                    <div class="footer-contacts">
+                        <div class="fc-row">
+                            <span class="fa fa-home"></span>
+                            Thomas More Kempen,<br/> 
+                            Geel, 2440
+                            Belgium
+                        </div>
+                        <div class="fc-row">
+                            <span class="fa fa-phone"></span>
+                            +32 14 80 22 10
+                        </div>
+                        <div class="fc-row">
+                            <span class="fa fa-envelope"></span>
+                            <strong>Karine Nicolay</strong><br>
+                            <a href="mailto:karine.nicolay@thomasmore.be">karine.nicolay@thomasmore.be</a>
+                        </div>                                    
+                    </div>
+
+                    
+
+                </div>
+                <!-- ./contacts -->
+
+            </div>
+
+        </div>
+        <!-- ./page footer holder -->
+    </div>
+    <!-- ./page footer wrap -->
+
+    <!-- page footer wrap -->
+    <div class="footer-wrap bg-darken-gray">
+        <!-- page footer holder -->
+        <div class="footer-holder">
+
+            <!-- copyright -->
+            <div class="copyright">
+                &copy; PHP-project by <a href="#">Echo 25</a> - All Rights Reserved  - Semih Doksanbir                          
+            </div>
+            <!-- ./copyright -->
+
+            <!-- social links -->
+            <div class="social-links">
+                <a href="https://www.facebook.com/pages/IC-Clear/"><span class="fa fa-facebook"></span></a>
+                <a href="https://twitter.com/icclear"><span class="fa fa-twitter"></span></a>
+             
+            </div>                        
+            <!-- ./social links -->
+
+        </div>
+        <!-- ./page footer holder -->
+    </div>
+    <!-- ./page footer wrap -->
+
+</footer>
+<!-- ./page footer -->
+
+</div>        
+<!-- ./page container -->
+</body>
+</html>
